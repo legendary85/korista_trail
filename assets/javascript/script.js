7d99116bc075463828b7e0c61f8b215188cb825f
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-08-01 17:11:00 -0500

    added a youtube.js file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -194,27 +194,6 @@ $(document).ready(function () {
 
     })
 
-    // google youTube API = AIzaSyA-3_5erpoBYArNCrepyTGpkTeL0HcmtkQ
-    // client id = 201425106177-o1mr3s3ql7dok9bh17q5gt2q514am9m5.apps.googleusercontent.com
-    //client secret = FdszwLbTRHFDuSD8FbKwvktx
-      // Load the IFrame Player API code asynchronously.
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/player_AIzaSyA-3_5erpoBYArNCrepyTGpkTeL0HcmtkQ";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  // Replace the 'ytplayer' element with an <iframe> and
-  // YouTube player after the API code downloads.
-  var player;
-  function onYouTubePlayerAPIReady() {
-    player = new YT.Player('player', {
-      height: '360',
-      width: '640',
-      videoId: 'M7lc1UVf-VE'
-    });
-  }
-
-
 
 
 })
