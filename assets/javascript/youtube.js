809d45595cf3e07140938f5a3d45adee58309149
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-08-02 01:58:55 -0500

    added youtube videos to video page

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,53 +1,20 @@
+ 
+ 
  $(document).ready(function(){
 
-   
-   // google youTube API = AIzaSyA-3_5erpoBYArNCrepyTGpkTeL0HcmtkQ
-   // client id = 201425106177-o1mr3s3ql7dok9bh17q5gt2q514am9m5.apps.googleusercontent.com
-   //client secret = FdszwLbTRHFDuSD8FbKwvktx
-   // Load the IFrame Player API code asynchronously.
-   var tag = document.createElement('script');
-   tag.src = "https://www.youtube.com/iframe_api";
-   var firstScriptTag = document.getElementsByTagName('script')[0];
-   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-   
-   // Replace the 'ytplayer' element with an <iframe> and
-   // YouTube player after the API code downloads.
-   var player;
-   function onYouTubePlayerAPIReady() {
-     player = new YT.Player('player', {
-       height: '360',
-       width: '640',
-       videoId: 'SVSf8fNp_kg',
-       playerVars: { 'rel': 0}
-      });
-    }
-    
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-      event.target.playVideo();
-    }
-    
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    
-    
-    function pauseVideo() {
-      player.pauseVideo();
-    }
-    
-    
-    function playVideo() {
-      player.playVideo();
-    }
-    
-    
-    function stopVideo() {
-      player.stopVideo();
-    }
-    
-    
-    function getPlayerState() {
-      return player.getPlayerState();
-    }
+  //variables initilized and declared
 
-  })   
+  var youtubeUrl ='https://www.youtube.com/watch?v=';
+  var bikeTips= ['OFoSU1ytTys','sZE8tJnTHhw','deuWgGzNCK8'];
+
+  for(var i = 0; i < bikeTips.length; i++){
+    var col = $('<div class="col-sm-6">')
+    var video = '<iframe width="560" height="315" src="https://www.youtube.com/embed/"' + bikeTips[i] + 'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+      col.append(video);
+      $('#videos').append(col);
+  }
+
+})    
+
+
+  
