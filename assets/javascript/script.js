12db1a50e6b5eec8fc9ea40e98808f40685d6353
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2019-08-02 05:11:05 -0500

    made videos in video tab responsive

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -203,17 +203,9 @@ $(document).ready(function () {
             // window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 15,cityid: '4684888',appid: 'fb0ce6d825db30974bf096625bf170a2',units: 'metric',containerid: 'openweathermap-widget-15',  });  
             // (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
             // var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
-
-
-
         })
-
-
-
     })
-
-    
-
+   
 
 
    
